f8ac4cdf036909d25e3fb1900919f773bf585cee
Lzimann <lzimann@users.noreply.github.com>
2017-02-19 00:47:13 -0300

    Fix some bad things that I've done

--- tgdb/index.php
+++ tgdb/index.php
@@ -1,22 +1,21 @@
 <?php 
 namespace tgdb;
-use DateTime;
 require_once("include/include.php");
 
 $user = auth();
 
 $tpl = new template("index", array(
-	"USERCKEY"	=> crossrefify,
+	"USERCKEY"	=> crossrefify($user[0], 'adminckey'),
 	"USERRANK"	=> $user[1],
-))
-$res = $mysqli->query("SELECT timestamp, text, adminckey, type FROM `".fmttable("messages")." WHERE type = 'memo' ORDER BY timestamp DESC");
+));
+$res = $mysqli->query("SELECT timestamp, text, adminckey, type FROM `".fmttable("messages")."` WHERE type = 'memo' ORDER BY timestamp DESC");
 
 $memos = array();
 while ($row = $res->fetch_assoc()) {
 	$memo = array();
 	$memo['DATE'] = $row['timestamp'];
 	$memo['ADMIN'] = $row['adminckey'];
-	$memo['MEMO'] = $row['text'];
+	$memo['MEMO'] = str_replace("\n", "\n<br/>", $row['text']);
 	
 	$memos[] = $memo;
 }
