fc20f16b553bf4a3e89dd399392f9927f4e6bd48
mozi_h <die-123-321@web.de>
2022-02-07 01:22:18 +0100

    Intro Text: Carousel and better links

--- src/templates/sections/introText.php
+++ src/templates/sections/introText.php
@@ -10,9 +10,45 @@
 			<p class="mt-2 mb-0">
 				Space Station 13 is a paranoia-laden round-based roleplaying game set against the backdrop of a nonsensical, metal death trap masquerading as a space station, with charming spritework designed to represent the sci-fi setting and its dangerous undertones. Have fun, and survive!
 			</p>
-			<div class="d-flex justify-content-center pt-2">
-				<a href="https://secure.byond.com/download/?invite=Tgstation" target="_blank">Get BYOND</a>
-				<a class="ms-4" href="https://tgstation13.org/wiki/Downloading_the_source_code#Hosting_a_server" target="_blank">Host a server</a>
+			<div id="screenshotCarousel" class="carousel slide my-2" data-bs-ride="carouselXX">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img src="./img/carousel/1.png" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-md-flex p-0 flex-row justify-content-center">
+							<div class="bg-dark-850 px-3 py-1">
+								A xenomorph infection
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img src="./img/carousel/2.png" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-md-flex p-0 flex-row justify-content-center">
+							<div class="bg-dark-850 px-3 py-1">
+								Miners setting out to get minerals
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img src="./img/carousel/3.png" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-md-flex p-0 flex-row justify-content-center">
+							<div class="bg-dark-850 px-3 py-1">
+								A normal shift in Chemistry
+							</div>
+						</div>
+					</div>
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#screenshotCarousel" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#screenshotCarousel" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>
+			</div>
+			<div class="d-flex justify-content-center">
+				<a href="https://tgstation13.org/wiki/Starter_guide" target="_blank">Starter Guide</a>
+				<a class="ms-4" href="https://secure.byond.com/download/?invite=Tgstation" target="_blank">Get BYOND</a>
 			</div>
 		</div>
 	</div>
